e6a9e8eebe16fa893aef91e0718c602b2667c1db
Lei, Lei <lei.lei@emc.com>
2021-11-08 00:00:56 +0800

    Update 263_easy_UglyNumber.cpp

--- src/263_easy_UglyNumber.cpp
+++ src/263_easy_UglyNumber.cpp
@@ -8,30 +8,14 @@
 
 class Solution {
 public:
-    bool isUgly(int num) {
-        if (num == 1) {
-            return true;
-        }
-            
-        if (num <= 0) {
-            return false;
-        }
-        
-        int result = num;
-        while (result % 2 == 0) {
-            result /= 2;
-        }
-        while (result % 3 == 0) {
-            result /= 3;
-        }
-        while (result % 5 == 0) {
-            result /= 5;
-        }
-        
-        if (result == 1) {
-            return true;
-        } else {
-            return false;
-        }
+    bool isUgly(int n) {
+        if (n<=0) return false;
+        while (n%2==0) n /= 2;
+        if (n==1) return true;
+        while (n%3==0) n /= 3;
+        if (n==1) return true;
+        while (n%5==0) n /= 5;
+        if (n==1) return true;
+        return false;
     }
 };
