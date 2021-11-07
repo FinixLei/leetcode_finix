dff8f4fdbd2725823d47d7bcbb15fdd34dda5828
Lei, Lei <lei.lei@emc.com>
2021-11-07 09:00:05 +0800

    26_easy_RemoveDuplicatesFromSortedArray.cpp

--- src/26_easy_RemoveDuplicatesFromSortedArray.cpp
+++ src/26_easy_RemoveDuplicatesFromSortedArray.cpp
@@ -42,17 +42,12 @@ public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.size() == 0) return 0;
         
-        int pos = 0;
-        int curr_value = nums[pos];
-        
+        int pos = 0;        
         for (int i=0; i<nums.size(); ++i) {
-            if (curr_value < nums[i]) {
-                pos += 1;
-                nums[pos] = nums[i];
-                curr_value = nums[i];
+            if (nums[pos] < nums[i]) {
+                nums[++pos] = nums[i];
             }
         }
-        
         return pos+1;
     }
 };
