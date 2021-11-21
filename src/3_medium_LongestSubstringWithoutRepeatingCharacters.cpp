8b87e83ff1141102aff4fe25595dded2fdd6dc5e
Lei, Lei <lei.lei@emc.com>
2021-11-21 19:33:40 +0800

    Add comments to 3_medium_LongestSubstringWithoutRepeatingCharacters.cpp

--- src/3_medium_LongestSubstringWithoutRepeatingCharacters.cpp
+++ src/3_medium_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -20,7 +20,13 @@ int lengthOfLongestSubstring(string s) {
     memset(bitmap, -1, bitmap_size * sizeof(int));
     
     for (int i=0; i<s.size(); i++) {
-        if (bitmap[s[i]] != -1 && last_hit < bitmap[s[i]]) {  // "abba", last_hit=1 but bitmap['a']=0, 1>0, so cannot assign bitmap['a'] to last_hit. 
+        // "abba", last_hit=1 but bitmap['a']=0, 1>0, so cannot assign bitmap['a'] to last_hit. 
+        // 下面其实是判断是否真的有重复数字出现。有重复数字出现需满足2个条件：
+        // 1. 位图中，该字符对应的值已经不是初始值
+        // 2. 上一次重复位置，在当前字符对应的位图值之前； 这是因为，上一次重复位置之后的位置，就是这一次计算的起始位置
+        // 当满足这2个条件时，即相当于发现了最新的重复字符，于是更新最新重复位置，并计算是否为最长无重复字符串
+        // 无论是否找到重复字符，都需要更新一下最长无重复字符串长度，以及更新当前字符的位图值。
+        if (bitmap[s[i]] != -1 && last_hit < bitmap[s[i]]) {  
             last_hit = bitmap[s[i]]; 
         } 
         longest = (i - last_hit > longest) ? i - last_hit : longest;
