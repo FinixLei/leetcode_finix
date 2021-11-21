23f01122f6d4499bf1325a62721d0793b67a0872
Lei, Lei <lei.lei@emc.com>
2021-11-21 20:05:28 +0800

    Update way-1 of 4_hard_MedianOfTwoSortedArrays.cpp

--- src/4_hard_MedianOfTwoSortedArrays.cpp
+++ src/4_hard_MedianOfTwoSortedArrays.cpp
@@ -8,39 +8,42 @@ Find the median of the two sorted arrays. The overall run time complexity should
 using namespace std;
 
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int size1 = nums1.size();
-    int size2 = nums2.size();
-    int size = size1 + size2;
-    
-    vector<int> result_array;
-    
-    for (int i=0, j=0; i<size1 || j<size2; ) {
-        if (i >= size1) {
-            result_array.push_back(nums2[j++]);
-            continue;
-        }
-        if (j >= size2) {
-            result_array.push_back(nums1[i++]);
-            continue;
-        }
-        
-        if (nums1[i] <= nums2[j]) {
-            result_array.push_back(nums1[i++]);
-        }
-        else {
-            result_array.push_back(nums2[j++]);
-        }
-    }
-    
-    if (size % 2 == 1) {
-        return result_array[(size-1)/2];
+    int m = nums1.size();
+    int n = nums2.size();
+    bool odd = (m+n) % 2;
+    pair<int, int> targetIndexes;
+    if (odd) {
+        targetIndexes = {(m+n-1)/2, (m+n-1)/2};
     }
     else {
-        
-        return (result_array[size/2 - 1] + result_array[size/2])/2.0; 
+        targetIndexes = {(m+n-1)/2, (m+n)/2};
     }
+    
+    int count=0, tmp=0;
+    int i=0, j=0, v1=0, v2=0;
+    
+    while (i<m || j<n) {
+        if (i >= m) {
+            tmp = nums2[j++];
+        }
+        else if (j >= n) {
+            tmp = nums1[i++];
+        }
+        else {  // both nums1 and nums2 are not empty 
+            tmp = min(nums1[i], nums2[j]);
+            if (nums1[i] < nums2[j]) i++;
+            else j++;
+        }
+        
+        if (count == targetIndexes.first) v1 = tmp;
+        if (count == targetIndexes.second) {
+            v2 = tmp;
+            break;
+        }
+        count ++;
+    }
+    return (v1+v2)*1.0/2; 
 }
-
 
 int main()
 {
@@ -57,6 +60,5 @@ int main()
     
     double result = findMedianSortedArrays(array1, array2);
     cout << "result = " << result << endl;
-    
     return 0;
 }
