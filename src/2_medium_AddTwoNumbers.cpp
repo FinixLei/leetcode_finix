9eb669d49b05d710afba923225f1f9628b44a29a
Lei, Lei <lei.lei@emc.com>
2021-11-21 18:09:21 +0800

    Update 2_medium_AddTwoNumbers.cpp

--- src/2_medium_AddTwoNumbers.cpp
+++ src/2_medium_AddTwoNumbers.cpp
@@ -17,45 +17,41 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if (l1 == NULL && l2 == NULL) {
-            return NULL;
-        }
-
-        int sum = 0;
-        if (l1 == NULL && l2 != NULL)
-            sum = l2->val;
-        else if (l1 != NULL && l2 == NULL)
-            sum = l1->val;
-        else
-            sum = l1->val + l2->val;
+        if (l1 == nullptr) return l2;
+        if (l2 == nullptr) return l1;
         
-        int cur_val = sum >= 10 ? sum - 10 : sum;
-        int step    = sum >= 10 ? 1 : 0;
-
-        ListNode * head = new ListNode(cur_val);
+        int inc = 0;
+        int tmp = l1->val + l2->val;
+        inc = tmp >= 10 ? 1 : 0;
+        tmp = tmp >= 10 ? (tmp-10) : tmp; 
+        
+        ListNode * head = new ListNode(tmp); 
         ListNode * p = head;
         l1 = l1->next;
         l2 = l2->next;
         
-        while (l1 != NULL || l2 != NULL) {
-            if (l1 == NULL && l2 != NULL) 
-                sum = l2->val + step;
-            else if (l1 != NULL && l2 == NULL) 
-                sum = l1->val + step;
-            else 
-                sum = l1->val + l2->val + step;
-        
-            cur_val = sum >= 10 ? sum - 10 : sum;
-            step    = sum >= 10 ? 1 : 0;
+        while (l1 || l2) {
+            if (l1 == nullptr) {
+                tmp = l2->val + inc;
+                l2 = l2->next;
+            }
+            else if (l2 == nullptr) {
+                tmp = l1->val + inc;
+                l1 = l1->next;
+            }
+            else {  // both are not nullptr
+                tmp = l1->val + l2->val + inc;
+                l1 = l1->next;
+                l2 = l2->next;
+            }
+            inc = tmp >= 10 ? 1 : 0;
+            tmp = tmp >= 10 ? (tmp-10) : tmp; 
             
-            p->next = new ListNode(cur_val); 
+            p->next = new ListNode(tmp);
             p = p->next;
-            if (l1 != NULL) l1 = l1->next;
-            if (l2 != NULL) l2 = l2->next;
         }
         
-        if (step > 0) p->next = new ListNode(step);
-        
+        if (inc > 0) p->next = new ListNode(inc);
         return head;
     }
 };
