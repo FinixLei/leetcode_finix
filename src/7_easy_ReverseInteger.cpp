fbf48de06b700007457ac724323d407501456974
Lei, Lei <lei.lei@emc.com>
2021-11-22 22:50:12 +0800

    Update 7_easy_ReverseInteger.cpp

--- src/7_easy_ReverseInteger.cpp
+++ src/7_easy_ReverseInteger.cpp
@@ -24,14 +24,11 @@ int reverse(int x) {
     if (!bPos ) x=-x;
     
     unsigned long result = 0;
-    
     while (x>0) {
         result = result *10 + x%10;
+        if (result > INT_MAX) return 0;
         x /= 10; 
     }
-    
-    if (result > INT_MAX) return 0;
-    
     return bPos ? result : (-result);
 }
 
