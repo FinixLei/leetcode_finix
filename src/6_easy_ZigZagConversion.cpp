4e262820ca0de409a3fed5f8b65613dd7abf5334
Lei, Lei <lei.lei@emc.com>
2021-11-22 22:38:08 +0800

    Update 6_easy_ZigZagConversion.cpp

--- src/6_easy_ZigZagConversion.cpp
+++ src/6_easy_ZigZagConversion.cpp
@@ -17,25 +17,28 @@ string convert(string s, int numRows) {
     if (numRows <= 1 || numRows >= size) return s;
     
     char new_array[numRows][size+1];
-    memset(&new_array[0][0], 0, sizeof(new_array));
-    
-    int pos[numRows]; 
-    memset(&pos[0], 0, sizeof(pos));
-
+    memset(&new_array[0][0], ' ', sizeof(new_array));
+    // for (int i=0; i<numRows; i++) new_array[i][size] = 0;
+ 
     int row = 0;
+    int col = 0;
     int step = 1;
     for (int i=0; i<size; i++) {
         if (row == numRows-1) step = -1;
         if (row == 0) step = 1;
         
-        new_array[row][pos[row]] = s[i];
-        pos[row] ++;
+        new_array[row][col] = s[i];
+        if (step == -1) col ++;
         row += step;
     }
     
-    string result = "";
+    string result;
     for (int i=0; i<numRows; i++) {
-        result += string(new_array[i]);
+        for (int j=0; j<size+1; j++) {
+            if (new_array[i][j] != ' ') {
+                result.push_back(new_array[i][j]);
+            }
+        }
     }
     
     return result;
