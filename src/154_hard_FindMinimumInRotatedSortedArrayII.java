e4c28e2b05953beb02e51b60b2160643e9d11173
leil3 <lei.lei@dell.com>
2023-12-09 11:35:29 +0800

    Add src/154_hard_FindMinimumInRotatedSortedArrayII.java, and updated src/153_medium_FindMinimumInRotatedSortedArray.java

--- src/154_hard_FindMinimumInRotatedSortedArrayII.java
+++ src/154_hard_FindMinimumInRotatedSortedArrayII.java
@@ -1,26 +1,19 @@
 /*
-Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
-[4,5,6,7,0,1,2] if it was rotated 4 times.
-[0,1,2,4,5,6,7] if it was rotated 7 times.
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
+[4,5,6,7,0,1,4] if it was rotated 4 times.
+[0,1,4,4,5,6,7] if it was rotated 7 times.
 Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
 
-Given the sorted rotated array nums of unique elements, return the minimum element of this array.
-You must write an algorithm that runs in O(log n) time.
+Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
+You must decrease the overall operation steps as much as possible.
 
 Example 1:
-    Input: nums = [3,4,5,1,2]
+    Input: nums = [1,3,5]
     Output: 1
-    Explanation: The original array was [1,2,3,4,5] rotated 3 times.
 
 Example 2:
-    Input: nums = [4,5,6,7,0,1,2]
+    Input: nums = [2,2,2,0,1]
     Output: 0
-    Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
-
-Example 3:
-    Input: nums = [11,13,15,17]
-    Output: 11
-    Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
 */
 
 class Solution {
