0ad9d93cb67dcd9be7288814d2b0fa70a35e2b5c
Finix Lei <zqleilei@163.com>
2026-07-24 18:20:44 +0800

    Update 58

--- src/58_easy_LengthOfLastWord.cpp
+++ src/58_easy_LengthOfLastWord.cpp
@@ -11,84 +11,23 @@ Example 2:
 Input: s = " "
 Output: 0
 */
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        const int size = s.size();
+        if (size == 0) return 0;
 
-#include <string>
-#include <iostream>
-using namespace std;
-
-int lengthOfLastWord(string& s) 
-{
-    int result = 0;
-    int last_index = s.size()-1;
-    int last_pos = -1;  // <0 means not started
-    
-    int i = last_index;
-    for (; i>=0; --i)
-    {
-        if (s[i] != ' ' && last_pos < 0) {
-            last_pos = i;
-            continue;
+        int i = size-1;
+        while (s[i] == ' ') {
+            i --;
         }
-        
-        if (s[i] == ' ' && last_pos > 0) {
-            result = last_pos - i;
-            break;
+
+        int count = 0;
+        while (i >= 0 && s[i] != ' ') {
+            count ++;
+            i --;
         }
-    }
-    
-    if (last_pos >= 0 && i < 0) {
-        result = last_pos + 1;
-    }
-    
-    return result; 
-}
 
-// way-2 
-int lengthOfLastWord_way_2(string& s) 
-{
-    if (s.size() == 0) return 0;
-    
-    size_t last_space_index = s.find_last_of(" ");
-    
-    if (last_space_index == string::npos) {
-        if (s[0] != ' ') return s.size();
-        else return 0;
-    }    
-    if (last_space_index < s.size()-1) {
-        return s.size()-1-last_space_index;
+        return count;
     }
-    
-    int i= last_space_index;
-    for (; i>=0; i--) {
-        if (s[i] != ' ') break;
-    }
-    if (i < 0) return 0;
-    
-    int result = 0;
-    while (i>=0 && s[i] != ' ' && i>=0) {
-        result ++;
-        i--;
-    }
-    
-    return result; 
-}
-
-int main()
-{
-    string s = "Hello World";
-    cout << s << ": \n" << lengthOfLastWord(s) << endl;
-    
-    s = " ";
-    cout << s << ": \n" << lengthOfLastWord(s) << endl;
-    
-    s = "a  ";
-    cout << s << ": \n" << lengthOfLastWord(s) << endl;
-    
-    s = "a";
-    cout << s << ": \n" << lengthOfLastWord(s) << endl;
-    
-    s = "  a";
-    cout << s << ": \n" << lengthOfLastWord(s) << endl;
-    
-    return 0;
-}
+};
