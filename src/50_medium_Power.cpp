63f6fe1830019616698eb34d101d08ac70240e37
FinixLei <zqleilei@163.com>
2026-09-01 13:07:16 +0800

    Update 50

--- src/50_medium_Power.cpp
+++ src/50_medium_Power.cpp
@@ -20,62 +20,20 @@ Note:
 n is a 32-bit signed integer, within the range [−2^31, 2^31−1]
 */
 
-#include <iostream>
-using namespace std;
+class Solution {
+private:
+    double quick_pow(double x, long N) {
+        if (N == 0) return 1.0;
+        if (N == 1) return x;
+        if (N < 0) throw std::runtime_error("wrong parameter");
 
-double myPow(double x, int n) {
-    if (n==0) return 1;
-    if (x==0) return 0;
-    if (x==1) return 1;
-    if (x==-1) {
-        if (n>0) {
-            if (n%2 == 0) return 1;
-            else return -1;
-        }
-        else {  // n < 0
-            if (n==-2147483648) return 1;
-            if ((-n)%2 == 0) return 1;
-            else return -1;
-        }
+        double y = quick_pow(x, N/2);
+        if (N % 2 == 0) return y*y;
+        else return y*y*x;
     }
-    if (n==-2147483648) return 0;
-   
-    bool pos_x = x>0 ? true : false;
-    bool pos_n = n>0 ? true : false;
-    bool overflow = false; 
-    
-    if (!pos_x) x = -x;
-    if (!pos_n) n = -n;
-    
-    double result = 1;
-    for (int i=0; i<n; i++) {
-        result *= x; 
-        if (result <= 0) {
-            overflow = true;
-            break;
-        }
-    }
-    
-    if (overflow) {
-        return 0;
-    }
-    
-    if (!pos_x && n%2 == 1) result = -result;    
-    return pos_n ? result : 1/result; 
-}
 
-int main()
-{
-    double result = 0;
-    
-    result = myPow(2, 10);
-    cout << "myPow(2, 10) = " << result << endl;
-    
-    result = myPow(-1, -2147483648);
-    cout << "myPow(-1, -2147483648) = " << result << endl;
-    
-    result = myPow(2, -2147483648);
-    cout << "myPow(2, -2147483648) = " << result << endl;
-
-    return 0;
-}
+public:
+    double myPow(double x, int n) {
+        return n >= 0 ? quick_pow(x, n) : 1.0 / quick_pow(x, -((long)(n)));
+    }
+};
