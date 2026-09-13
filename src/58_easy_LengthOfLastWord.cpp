8a1334e3eeb8bdda30d0ad25d9cae4fd7ab8378e
FinixLei <zqleilei@163.com>
2026-09-13 20:32:12 +0800

    Update 58

--- src/58_easy_LengthOfLastWord.cpp
+++ src/58_easy_LengthOfLastWord.cpp
@@ -18,16 +18,13 @@ public:
         if (size == 0) return 0;
 
         int i = size-1;
-        while (s[i] == ' ') {
-            i --;
-        }
+        while (s[i] == ' ') --i;
 
         int count = 0;
         while (i >= 0 && s[i] != ' ') {
             count ++;
             i --;
         }
-
         return count;
     }
 };
